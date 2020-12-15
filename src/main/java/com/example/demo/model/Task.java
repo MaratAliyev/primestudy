0f35dd82ca9d230ace917bb70656683a6cd86cf9
Марат Алиев <m.aliyev@primesource.local>
2020-12-15 17:44:15 +0600

    first commi222t

--- src/main/java/com/example/demo/model/Task.java
+++ src/main/java/com/example/demo/model/Task.java
@@ -19,4 +19,35 @@ public class Task {
     private String description;
     private boolean done;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
